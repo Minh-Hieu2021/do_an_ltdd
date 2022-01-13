aea548fd78ed9d3c73089b9dec7b4617a5d66c98
VietDuc20 <96774127+VietDuc20@users.noreply.github.com>
2022-01-13 12:58:37 +0700

    Update product_model.dart
    thêm mô tả cho model của sản phẩm

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,14 +1,15 @@
 class Product {
   final String image;
-  final String title;
+  final String title, description;
   final int price, id;
   Product({
     required this.image,
     required this.title,
     required this.price,
     required this.id,
-  });
-}
+    required this.description,
+  });             
+  }       
 
 final List<Product> products = [
   Product(
@@ -16,47 +17,55 @@ final List<Product> products = [
     image: "assets/images/sp1.png",
     title: "Acer Nitro 5",
     price: 25000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
   Product(
     id: 2,
     image: "assets/images/sp1.png",
     title: "Asus Gaming NS6",
     price: 27000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
   Product(
     id: 3,
     image: "assets/images/sp1.png",
     title: "Gaming HP VICTUS 16",
     price: 30000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
   Product(
     id: 4,
     image: "assets/images/sp1.png",
     title: "Gaming MSI Katana",
     price: 40000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
   Product(
     id: 5,
     image: "assets/images/sp1.png",
     title: "Gaming Asus ROG Strix",
     price: 15000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
   Product(
     id: 3,
     image: "assets/images/sp1.png",
     title: "Gaming HP VICTUS 16",
     price: 30000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
   Product(
     id: 4,
     image: "assets/images/sp1.png",
     title: "Gaming MSI Katana",
     price: 40000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
   Product(
     id: 5,
     image: "assets/images/sp1.png",
     title: "Gaming Asus ROG Strix",
     price: 15000000,
+    description: "Nhà sản xuất : ASUS\nXuất xứ : Chính hãng\nBảo hành : 24 Tháng\nTình trạng : Mới 100%",
   ),
 ];
